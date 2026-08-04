464c1fea08d5dc5d2c94b5b8f1e536479250f742
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-08-04 17:41:47 -0400

    Adding database user models with load user logic

--- Code/main.py
+++ Code/main.py
@@ -4,7 +4,10 @@ from dotenv import load_dotenv
 from flask_login import (LoginManager, current_user, login_required, login_user, logout_user)
 
 from config import app
-from models import db, User
+from models import db, User, Admin, Resident
+
+from sqlalchemy import Date, DateTime
+from datetime import date, datetime
 
 login_manager = LoginManager()
 login_manager.login_view = "login"
@@ -13,10 +16,33 @@ login_manager.init_app(app)
 
 with app.app_context():
     db.create_all()
+    
+    # For creating Resident
+    # string_date = "2026-08-04"
+    # dt = datetime.strptime(string_date,  "%Y-%m-%d")
+    # resident1 = Resident(name="Thomas Train", email="ttrain@email.com", apartment="101A", locker_location="2-100", lease_date=dt.date(), parking_status="paid")
+
+    # resident1.set_password("password")
+
+    # db.session.add(resident1)
+    # db.session.commit()
+
+    # resident = Resident.query.filter_by(email="ttrain@email.com").first()
+
+    # print(resident, resident.name, resident.locker_location)
 
 @login_manager.user_loader
-def load_user(user_id):
-    return db.session.get(User, int(user_id))
+def load_user(polymorphic_id):
+    if not polymorphic_id or ":" not in polymorphic_id:
+        return None
+
+    user_role, user_id = polymorphic_id.split(":", 1)
+
+    if user_role == "admin":
+        return db.session.get(Admin, int(user_id))
+
+    if user_role == "resident":
+        return db.session.get(Resident, int(user_id))
 
 @app.route("/")
 def home():
--- Code/models.py
+++ Code/models.py
@@ -1,6 +1,13 @@
 from flask_login import UserMixin
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import (check_password_hash, generate_password_hash)
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Date, DateTime
+from datetime import date
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+class Base(DeclarativeBase):
+    pass
 
 db = SQLAlchemy()
 
@@ -8,8 +15,101 @@ db = SQLAlchemy()
 # Admin, Resident
 # Building_info, Announcement, Post
 
-# Building off google ai overview 
-class User(db.Model):
-    __abstract__ = True
+# Building off SQLAlchemy ORM Inheritance documentation
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
 
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    name: Mapped[str] = mapped_column(
+        db.String(100), 
+        nullable=False)
+
+    email: Mapped[str] = mapped_column(
+        db.String(255),
+        unique=True,
+        nullable = False
+    )
+
+    password_hash: Mapped[str] = mapped_column(
+        db.String(255),
+        nullable = False
+    )
+
+    role: Mapped[str]
+
+    __mapper_args__ = {
+        # "polymorphic_identity": "user",
+        "polymorphic_on": "role",
+    }
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def is_admin(self):
+        if self.role == "admin":
+            return True
+        
+        return False
+
+    def __repr__(self):
+        # returns the class name and the user's id
+        return f"<{self.__class__.__name__}:{self.id}>"
     
+    def get_id(self):
+        return f"{self.__class__.__name__}:{self.id}"
+
+class Admin(User):
+    __tablename__ = "admins"
+
+    id: Mapped[int] = mapped_column(db.ForeignKey("users.id"), primary_key=True)
+    residents: Mapped[list["Resident"]] = relationship()
+
+    __mapper_args__ = {
+        "polymorphic_identity": "admin"
+    }
+
+class Resident(User):
+    __tablename__ = "residents"
+
+    id: Mapped[int] = mapped_column(db.ForeignKey("users.id"), primary_key=True)
+    
+    apartment: Mapped[str] = mapped_column(
+        #example 101A -> 100s = floor 1, 01 = room number, A = building side
+        db.String(4),
+        unique=True,
+        nullable=False
+    )
+
+    locker_location: Mapped[str] = mapped_column(
+        # example "2-100" -> second floor, locker number 100
+        db.String(5),
+        unique=True,
+        nullable=False
+    )
+
+    lease_date: Mapped[date] = mapped_column(
+        # format: YYYY-MM-DD
+        db.Date(),
+        nullable=False
+    )
+
+    parking_status: Mapped[str] = mapped_column(
+        # paid, late, revoked, inactive
+        db.String(8),
+        nullable=False,
+    )
+    
+    # admin_id: Mapped[int] = mapped_column(
+    #     db.Integer,
+    #     db.ForeignKey("admin.id"),
+    #     nullable=False
+    # )
+    # admin: Mapped["Admin"] = relationship(back_populates="residents", uselist=False)
+
+    __mapper_args__ = {
+        "polymorphic_identity": "resident"
+    }
